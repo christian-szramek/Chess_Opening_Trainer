8c905f2a4de2458ee7b057387adb8a46bfcc0ee9
Christian Szramek <christian.szramek@web.de>
2024-10-17 12:39:28 +0200

    - changed auto generated ids to manual ids and start from 1

--- Chess_Opening_Trainer_Backend/src/main/java/com/christianszramek/Chess_Opening_Trainer_Backend/model/Opening.java
+++ Chess_Opening_Trainer_Backend/src/main/java/com/christianszramek/Chess_Opening_Trainer_Backend/model/Opening.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Table(name="openings")
 public class Opening {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private String eco;
